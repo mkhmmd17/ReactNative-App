edfdb2c226dcd795d4490bb0b62fd78da2e2ae31
mkhmmd17 <maxmelhi95@gmail.com>
2025-01-07 19:12:03 -0500

    Style Sheet example

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Last dance!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>
+          Another piece of text
+        </Text>  
+      </View>
+      <Text style={styles.dummyText}>
+        Hello World!
+      </Text>
+      <Button title='Click me!' />
     </View>
   );
 }
@@ -17,4 +23,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding: 16
+  }
 });
